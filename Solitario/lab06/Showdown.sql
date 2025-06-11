4119c3a5a075492f3f3d088304aa48573468205b
Sebastian <sdavidp@ulasalle.edu.pe>
2025-06-11 00:43:49 -0500

    AL FIN EL SQL ODIO NO SABER CERRAR PROCESOS TARDE MAS DE 1 HORA AAAAAAA

---
+++ Solitario/lab06/Showdown.sql
@@ -0,0 +1,63 @@
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19  Distrib 10.11.11-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: Showdown
+-- ------------------------------------------------------
+-- Server version	10.11.11-MariaDB-0+deb12u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `sports`
+--
+
+DROP TABLE IF EXISTS `sports`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sports` (
+  `Sports` varchar(30) DEFAULT NULL,
+  `Adminstracion` int(16) DEFAULT NULL,
+  `Derecho` int(16) DEFAULT NULL,
+  `Psicologia` int(16) DEFAULT NULL,
+  `Software` int(16) DEFAULT NULL,
+  `C_Comunicacion` int(16) DEFAULT NULL,
+  `Ing_Comercial` int(16) DEFAULT NULL,
+  `Arquitectura_Industrual` int(16) DEFAULT NULL,
+  `Personal` int(16) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sports`
+--
+
+LOCK TABLES `sports` WRITE;
+/*!40000 ALTER TABLE `sports` DISABLE KEYS */;
+INSERT INTO `sports` VALUES
+('Futsal Varones',15,16,7,3,0,1,1,5),
+('Basket Varones',57,59,12,11,NULL,10,33,NULL),
+('Futsal Damas',5,15,13,NULL,2,0,1,NULL),
+('Basquet Damas',1,0,0,0,0,0,NULL,NULL),
+('Voley Mixto',6,2,0,4,0,2,1,NULL);
+/*!40000 ALTER TABLE `sports` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-06-11 13:37:16
